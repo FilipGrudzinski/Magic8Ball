836db469c56c8615bca038aac8b63325f3664461
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-09-19 13:37:04 +0200

    Magic 8 Ball App

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
+    @IBOutlet weak var askButtonView: UIButton!
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        askButtonView.layer.cornerRadius = 8
+        askButtonView.layer.masksToBounds = true
     }
 
 
+    @IBAction func askButton(_ sender: Any) {
+        
+        rollAnswer()
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        rollAnswer()
+        
+    }
+    
+    func rollAnswer(){
+        
+        let answer = Int.random(in: 0 ... 4)
+        print(ballArray[answer])
+        
+        ballImage.image = UIImage(named: ballArray[answer])
+        
+    }
+    
+    
 }
 
