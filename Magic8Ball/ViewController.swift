d690530b2c8d84727c5f3b089f59ce77eb948bf1
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-09-19 13:02:46 +0200

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Filip on 19.09.2018.
+//  Copyright © 2018 Filip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
